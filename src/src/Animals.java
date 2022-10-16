2a80563c56415bb404c97f82af1e85430ea179cf
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-10-16 17:13:43 +0300

    Added final field tail. Added field paws to Animals.java

--- src/src/Animals.java
+++ src/src/Animals.java
@@ -1,7 +1,14 @@
 abstract public class Animals {
+
     public abstract String voise();
+    final public int tail;
+    public int paws;
 
 
+    public Animals(int paws, int tail) {
+        this.paws = paws;
+        this.tail = tail;
+    }
 
     public static void main(String[] args) {
 
