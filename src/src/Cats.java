58406df13e1e143a98bd01d76dcabb0ea951bf1c
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-10-16 17:28:09 +0300

    Created two classes Dogs and Cats that inherit a previouslt class Animals

--- src/src/Cats.java
+++ src/src/Cats.java
@@ -1,2 +1,12 @@
-public class Cats {
+public class Cats extends Animals {
+
+    public Cats(int paws, int tail) {
+        super(paws, tail);
+    }
+
+    @Override
+    public String voise() {
+        System.out.println("Cats says: няв!");
+        return null;
+    }
 }
--- src/src/Dogs.java
+++ src/src/Dogs.java
@@ -1,2 +1,12 @@
-public class Dogs {
+public class Dogs extends Animals {
+
+    public Dogs(int paws, int tail) {
+        super(paws, tail);
+    }
+
+    @Override
+    public String voise() {
+        System.out.println("Dogs says: гав-гав!");
+        return null;
+    }
 }
